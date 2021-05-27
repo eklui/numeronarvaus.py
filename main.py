74af180fa7003f550f861d37393ec6f15d720a09
Eelis Keränen <>
2021-05-27 10:51:45 +0000

    Lintteröinti

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from random import randint
 
 arvaus = input("Arvaa numero nollan ja sadan väliltä: ")
-numero = randint(0,100)
+numero = randint(0, 100)
 
 while int(arvaus) < numero or int(arvaus) > numero:
-  if int(arvaus) < numero:
-    print("Numero on suurempi ")
-    arvaus = input()
-  else:
-    print("Numero on pienempi" )
-    arvaus = input()
+	if int(arvaus) < numero:
+		print("Numero on suurempi ")
+		arvaus = input()
+	else:
+		print("Numero on pienempi")
+		arvaus = input()
 
 else:
-  print("Voitit pelin")
-  
-  
-
-
-
+	print("Voitit pelin")

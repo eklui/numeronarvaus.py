30bc4923af30d869ffdae24349fd6fc89c854193
EelisKernen <>
2021-05-27 10:50:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from random import randint
+
+arvaus = input("Arvaa numero nollan ja sadan väliltä: ")
+numero = randint(0,100)
+
+while int(arvaus) < numero or int(arvaus) > numero:
+  if int(arvaus) < numero:
+    print("Numero on suurempi ")
+    arvaus = input()
+  else:
+    print("Numero on pienempi" )
+    arvaus = input()
+
+else:
+  print("Voitit pelin")
+  
+  
+
+
+
